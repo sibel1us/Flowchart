41cfd225063406905d7cd1bea63dfd94db5ac9f4
Sipi Ovaska <sipi@live.fi>
2018-11-24 22:16:07 +0200

    Add Diagram and Node controls
    -Begin adding drag/drop function

---
+++ Flowchart/Controls/Diagram.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flowchart
+{
+    /// <summary>
+    /// Interaction logic for Diagram.xaml
+    /// </summary>
+    [ContentProperty(nameof(Children))]
+    public partial class Diagram : UserControl
+    {
+        public Node DraggedNode { get; set; }
+
+        public event EventHandler<DiagramGridChangedEventArgs> DiagramSizeChanged;
+
+        /// <summary>
+        /// Children of the root grid.
+        /// </summary>
+        public UIElementCollection Children
+        {
+            get { return (UIElementCollection)GetValue(ChildrenProperty.DependencyProperty); }
+            private set { SetValue(ChildrenProperty, value); }
+        }
+
+        /// <summary>
+        /// Amount of grid rows in the diagram.
+        /// </summary>
+        public int Rows
+        {
+            get => RootGrid.RowDefinitions.Count;
+            set
+            {
+                if (value < 1)
+                    return;
+
+                // TODO
+                if (value > 64)
+                    return;
+
+                int delta = value - Rows;
+                if (delta == 0) return;
+                DiagramSizeChanged?.Invoke(this, new DiagramGridChangedEventArgs(delta, 0));
+
+                if (delta > 0)
+                    for (int i = 0; i < delta; i++)
+                        RootGrid.RowDefinitions.Add(new RowDefinition());
+
+                else
+                    for (int i = 0; i < -delta; i++)
+                        RootGrid.RowDefinitions.RemoveAt(Rows - 1);
+            }
+        }
+
+        /// <summary>
+        /// Amount of grid columns in the diagram.
+        /// </summary>
+        public int Columns
+        {
+            get => RootGrid.ColumnDefinitions.Count;
+            set
+            {
+                if (value < 1)
+                    return;
+
+                // TODO
+                if (value > 64)
+                    return;
+
+                int delta = value - Columns;
+                if (delta == 0) return;
+                DiagramSizeChanged?.Invoke(this, new DiagramGridChangedEventArgs(0, delta));
+
+                if (delta > 0)
+                    for (int i = 0; i < delta; i++)
+                        RootGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                else
+                    for (int i = 0; i < -delta; i++)
+                        RootGrid.ColumnDefinitions.RemoveAt(Columns - 1);
+            }
+        }
+
+        public Diagram()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Children = RootGrid.Children;
+        }
+
+        // Source: https://stackoverflow.com/a/9186486/
+        public static readonly DependencyPropertyKey ChildrenProperty =
+            DependencyProperty.RegisterReadOnly(
+                nameof(Children),
+                typeof(UIElementCollection),
+                typeof(Diagram),
+                new PropertyMetadata());
+
+        private void RootGrid_DragEnter(object sender, DragEventArgs e)
+        {
+            Node node = (Node)e.Data.GetData(nameof(Node));
+
+            UpdatePreviewPosition(e);
+        }
+
+        private void RootGrid_DragOver(object sender, DragEventArgs e)
+        {
+            UpdatePreviewPosition(e);
+        }
+
+        private void RootGrid_DragLeave(object sender, DragEventArgs e)
+        {
+        }
+
+        private void RootGrid_Drop(object sender, DragEventArgs e)
+        {
+            Node node = (Node)e.Data.GetData(nameof(Node));
+            Point gridPosition = GetPositionInGrid(e.GetPosition(RootGrid));
+
+            node.Column = (int)gridPosition.X - (node.ColumnSpan - 1);
+            node.Row = (int)gridPosition.Y;
+        }
+
+        private void UpdatePreviewPosition(DragEventArgs e)
+        {
+            Node node = (Node)e.Data.GetData(nameof(Node));
+            Point gridPosition = GetPositionInGrid(e.GetPosition(RootGrid));
+
+            node.Column = (int)gridPosition.X - (node.ColumnSpan - 1);
+            node.Row = (int)gridPosition.Y;
+            node.ColumnSpan = node.ColumnSpan;
+            node.RowSpan = node.RowSpan;
+        }
+
+        /// <summary>
+        /// Gets the row and column of <paramref name="point"/> in <see cref="RootGrid"/>.
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        private Point GetPositionInGrid(Point point)
+        {
+            int row = 0;
+            int column = 0;
+            double accumulatedHeight = 0.0;
+            double accumulatedWidth = 0.0;
+
+            foreach (var rowDefinition in RootGrid.RowDefinitions)
+            {
+                accumulatedHeight += rowDefinition.ActualHeight;
+                if (accumulatedHeight >= point.Y)
+                    break;
+                row++;
+            }
+
+            foreach (var columnDefinition in RootGrid.ColumnDefinitions)
+            {
+                accumulatedWidth += columnDefinition.ActualWidth;
+                if (accumulatedWidth >= point.X)
+                    break;
+                column++;
+            }
+
+            return new Point(column, row);
+        }
+    }
+
+    /// <summary>
+    /// Provides data on the change whenever <see cref="Diagram"/>'s dimensions are changed.
+    /// </summary>
+    public class DiagramGridChangedEventArgs
+    {
+        public DiagramGridChangedEventArgs(int rowDelta, int columnDelta)
+        {
+            this.RowDelta = rowDelta;
+            this.ColumnDelta = columnDelta;
+        }
+
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+    }
+}
---
+++ Flowchart/Controls/Node.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flowchart
+{
+    /// <summary>
+    /// Interaction logic for Node.xaml
+    /// </summary>
+    public partial class Node : UserControl
+    {
+        public event EventHandler<NodeDimensionsChangedEventArgs> NodeDimensionsChanged;
+        public event EventHandler<NodePositionChangedEventArgs> NodePositionChanged;
+
+        public Diagram Diagram => (Diagram)((Grid)this.Parent).Parent;
+
+        public bool IsDraggable { get; set; } = true;
+        public bool IsDragged
+        {
+            get => Diagram.DraggedNode == this;
+            set => Diagram.DraggedNode = (value ? this : null);
+        }
+
+        public int Row
+        {
+            get => Grid.GetRow(this);
+            set
+            {
+                if (value < 0)
+                    return;
+
+                // TODO
+                if (value >= 64)
+                    return;
+
+                int delta = value - Row;
+                if (delta == 0) return;
+                NodePositionChanged?.Invoke(this, new NodePositionChangedEventArgs(delta, 0));
+                Grid.SetRow(this, value);
+            }
+        }
+
+        public int Column
+        {
+            get => Grid.GetColumn(this);
+            set
+            {
+                if (value < 0)
+                    return;
+
+                // TODO
+                if (value >= 64)
+                    return;
+
+                int delta = value - Column;
+                if (delta == 0) return;
+                NodePositionChanged?.Invoke(this, new NodePositionChangedEventArgs(0, delta));
+                Grid.SetColumn(this, value);
+            }
+        }
+
+        public int ColumnSpan
+        {
+            get => Grid.GetColumnSpan(this);
+            set
+            {
+                if (value < 1)
+                    return;
+
+                // TODO
+                if (value > 64)
+                    return;
+
+                int delta = value - ColumnSpan;
+                if (delta == 0) return;
+                NodeDimensionsChanged?.Invoke(this, new NodeDimensionsChangedEventArgs(0, delta));
+                Grid.SetColumnSpan(this, value);
+            }
+        }
+
+        public int RowSpan
+        {
+            get => Grid.GetRowSpan(this);
+            set
+            {
+                if (value < 1)
+                    return;
+
+                // TODO
+                if (value > 64)
+                    return;
+
+                int delta = value - RowSpan;
+                if (delta == 0) return;
+                NodeDimensionsChanged?.Invoke(this, new NodeDimensionsChangedEventArgs(0, delta));
+                Grid.SetRowSpan(this, value);
+            }
+        }
+
+        public Brush BackgroundColor
+        {
+            get => Root.Background;
+            set => Root.Background = value;
+        }
+
+        public Node()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Anchor_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsDraggable && !IsDragged && e.LeftButton == MouseButtonState.Pressed)
+            {
+                IsDragged = true;
+                Opacity = 0.5;
+
+                DataObject data = new DataObject();
+                data.SetData(nameof(Node), this);
+                DragDropEffects result = DragDrop.DoDragDrop(this, data, DragDropEffects.Move);
+
+                Opacity = 16;
+                IsDragged = false;
+            }
+        }
+    }
+
+    public class NodeDimensionsChangedEventArgs
+    {
+        public NodeDimensionsChangedEventArgs(int rowDelta, int columnDelta)
+        {
+            this.RowDelta = rowDelta;
+            this.ColumnDelta = columnDelta;
+        }
+
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+    }
+
+    public class NodePositionChangedEventArgs
+    {
+        public NodePositionChangedEventArgs(int rowDelta, int columnDelta)
+        {
+            this.RowDelta = rowDelta;
+            this.ColumnDelta = columnDelta;
+        }
+
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+    }
+}
