ac8d9f381a971c124c021d749c1b7791ff9a65a7
Sipi Ovaska <sipi@live.fi>
2018-11-25 02:21:20 +0200

    Add Settings file

--- Flowchart/Properties/Settings.Designer.cs
+++ Flowchart/Properties/Settings.Designer.cs
@@ -8,22 +8,102 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Flowchart.Properties
-{
-
-
+namespace Flowchart.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.7")]
+        public double InactiveNodeOpacity {
+            get {
+                return ((double)(this["InactiveNodeOpacity"]));
+            }
+            set {
+                this["InactiveNodeOpacity"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public double NodeOpacity {
+            get {
+                return ((double)(this["NodeOpacity"]));
+            }
+            set {
+                this["NodeOpacity"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("64")]
+        public int MaxDiagramWidth {
+            get {
+                return ((int)(this["MaxDiagramWidth"]));
+            }
+            set {
+                this["MaxDiagramWidth"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("64")]
+        public int MaxDiagramHeight {
+            get {
+                return ((int)(this["MaxDiagramHeight"]));
+            }
+            set {
+                this["MaxDiagramHeight"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("64")]
+        public int MaxNodeWidth {
+            get {
+                return ((int)(this["MaxNodeWidth"]));
+            }
+            set {
+                this["MaxNodeWidth"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("64")]
+        public int MaxNodeHeight {
+            get {
+                return ((int)(this["MaxNodeHeight"]));
+            }
+            set {
+                this["MaxNodeHeight"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.4")]
+        public double InvalidNodeOpacity {
+            get {
+                return ((double)(this["InvalidNodeOpacity"]));
+            }
+            set {
+                this["InvalidNodeOpacity"] = value;
             }
         }
     }
